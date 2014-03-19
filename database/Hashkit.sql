93b9859e6a6bced39da40f09041f65087f6ed250
Aik Chun <aikchun616@gmail.com>
2014-03-19 13:09:06 +0800

    exported database

--- database/Hashkit.sql
+++ database/Hashkit.sql
@@ -5,27 +5,27 @@ SET foreign_key_checks = 0;
 SET time_zone = '-07:00';
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
-DROP TABLE IF EXISTS `hashnames`;
-CREATE TABLE `hashnames` (
+DROP TABLE IF EXISTS `hash_algorithm`;
+CREATE TABLE `hash_algorithm` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
-  `name` varchar(20) NOT NULL,
+  `name` text NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `hashnames` (`id`, `name`) VALUES
+INSERT INTO `hash_algorithm` (`id`, `name`) VALUES
 (1,	'SHA-1'),
-(2,	'MD2'),
-(3,	'MD4'),
-(4,	'MD5'),
+(2,	'MD5'),
+(3,	'MD2'),
+(4,	'MD4'),
 (5,	'SHA-256');
 
-DROP TABLE IF EXISTS `hashprocesses`;
-CREATE TABLE `hashprocesses` (
+DROP TABLE IF EXISTS `hash_result`;
+CREATE TABLE `hash_result` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `plaintext` text NOT NULL,
-  `message_digest` int(11) NOT NULL,
+  `message_digest` text NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
--- 2014-03-18 02:06:52
+-- 2014-03-18 22:07:19
