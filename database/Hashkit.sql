630d8155bff99a50f35523259c40d7fb83d17b19
Aik Chun <aikchun616@gmail.com>
2014-03-20 02:31:38 +0800

    exported latest database

--- database/Hashkit.sql
+++ database/Hashkit.sql
@@ -5,6 +5,21 @@ SET foreign_key_checks = 0;
 SET time_zone = '-07:00';
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
+DROP DATABASE IF EXISTS `Hashkit`;
+CREATE DATABASE `Hashkit` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `Hashkit`;
+
+DROP TABLE IF EXISTS `group`;
+CREATE TABLE `group` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `group` (`id`, `name`) VALUES
+(1,	'administrator'),
+(2,	'guest');
+
 DROP TABLE IF EXISTS `hash_algorithm`;
 CREATE TABLE `hash_algorithm` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
@@ -13,11 +28,11 @@ CREATE TABLE `hash_algorithm` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT INTO `hash_algorithm` (`id`, `name`) VALUES
-(1,	'SHA-1'),
+(1,	'SHA1'),
 (2,	'MD5'),
 (3,	'MD2'),
 (4,	'MD4'),
-(5,	'SHA-256');
+(5,	'SHA256');
 
 DROP TABLE IF EXISTS `hash_result`;
 CREATE TABLE `hash_result` (
@@ -28,4 +43,18 @@ CREATE TABLE `hash_result` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
--- 2014-03-18 22:07:19
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `password` varchar(25) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `group_id` int(10) NOT NULL,
+  `profile` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `user` (`id`, `password`, `name`, `email`, `group_id`, `profile`) VALUES
+(1,	'5baa61e4c9b93f3f0682250b6',	'aikchun',	'aikchun616@gmail.com',	1,	'HI there. I\'m the first user.');
+
+-- 2014-03-19 11:30:31
