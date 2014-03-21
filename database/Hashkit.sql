feff9a8e3ac9ae3fa83ee7f012478ee3b1d8b704
Aik Chun <aikchun616@gmail.com>
2014-03-21 12:41:16 +0800

    Exported database

--- database/Hashkit.sql
+++ database/Hashkit.sql
@@ -47,7 +47,16 @@ CREATE TABLE `hash_result` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT INTO `hash_result` (`id`, `plaintext`, `message_digest`, `hash_algorithm_id`, `user_id`) VALUES
-(3,	'asd',	'61118995d26bef582a59dec9220483e8 ',	0,	1);
+(1,	'password',	'5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8',	1,	1),
+(2,	'password',	'5f4dcc3b5aa765d61d8327deb882cf99',	2,	1),
+(3,	'password',	'f03881a88c6e39135f0ecc60efd609b9',	3,	1),
+(4,	'password',	'8a9d093f14f8701df17732b2bb182c74',	4,	1),
+(5,	'password',	'5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8',	5,	1),
+(6,	'123456',	'7c4a8d09ca3762af61e59520943dc26494f8941b',	1,	1),
+(7,	'123456',	'e10adc3949ba59abbe56e057f20f883e',	2,	1),
+(8,	'123456',	'd4541250b586296fcce5dea4463ae17f',	3,	1),
+(9,	'123456',	'585028aa0f794af812ee3be8804eb14a',	4,	1),
+(10,	'123456',	'8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92',	5,	1);
 
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
@@ -63,4 +72,4 @@ CREATE TABLE `user` (
 INSERT INTO `user` (`id`, `password`, `name`, `email`, `group_id`, `profile`) VALUES
 (1,	'5baa61e4c9b93f3f0682250b6',	'aikchun',	'aikchun616@gmail.com',	1,	'HI there. I\'m the first user.');
 
--- 2014-03-20 02:51:15
+-- 2014-03-20 21:40:31
