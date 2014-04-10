57e2f24c21a9a3e355451aca8fb68e809fec10c3
aikchun <aikchun@Y580.(none)>
2014-04-10 10:35:37 +0800

    ref #7 Added Lib class for sending reset password email.

---
+++ app/Lib/Email/PasswordEmail.php
@@ -0,0 +1,70 @@
+<?php
+App::uses('CakeEmail', 'Network/Email');
+
+class PasswordEmail {
+
+	private $emailConfig = 'gmail';
+	private $sender = 'support@hashkit.com';
+	private $hashkit = 'support@hashkit.com';
+
+	private $from;
+	private $to;
+
+	private $email;
+	private $admin = false;
+
+	public function __construct($to, $admin=false) {
+		$this->from	= array('full_name' => 'Hashkit', 'email' => $this->hashkit);
+		$this->to	= $to;
+		$this->admin = $admin;
+
+		$this->_prepareAddressFields();
+	}
+
+	protected function _prepareAddressFields() {
+		$fromEmail	= $this->from['email'];
+		$fromFullName	= $this->from['full_name'];
+
+		$toEmail	= $this->to['email'];
+		$toFullName	= $this->to['full_name'];
+
+		$email = new CakeEmail($this->emailConfig);
+
+		$email->from(array($fromEmail => $fromFullName));
+		$email->to(array($toEmail => $toFullName));
+		$email->sender($this->sender);
+		$email->replyTo($fromEmail, $fromFullName);
+		$this->email = $email;
+		return $email;
+	}
+
+	public function sendNewPassword($newPassword) {
+		$email = $this->email;
+		$email->subject('[Password Reset] Do NOT reply to this email');
+
+		if (EMAIL_ON) {
+			$result = $email->send("This is your new password $newPassword.");
+		} else {
+			$result = $email;
+		}
+		return $result;
+	}
+
+	public function sendToken($token) {
+		$email = $this->email;
+		$email->subject('[Password Reset] Do NOT reply to this email');
+		if ($this->admin) {
+			$prefix = '/admin';
+		} else {
+			$prefix = '';
+		}
+		$baseURL = Router::url($prefix . '/users/reset_password?token=' . $token, true);
+
+		if (EMAIL_ON) {
+			$result = $email->send("Please click this link to reset your password. $baseURL");
+		} else {
+			$result = $email;
+		}
+		return $result;
+	}
+}
