ea79d8e13935e974b9baa4ac081e36a26c063380
ian <ian@ian-VirtualBox.(none)>
2014-04-20 21:35:09 +0800

    update error

--- app/Model/Description.php
+++ app/Model/Description.php
@@ -11,7 +11,7 @@ class Description extends AppModel {
  *
  * @var mixed False or table name
  */
-	public $useTable = 'description';
+	public $useTable = 'Description';
 
 	public $hasMany = array(
 		'HashResult' => array(
