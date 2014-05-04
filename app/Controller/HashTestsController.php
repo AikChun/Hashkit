196e211deee853c735675d8326b77ab53da32b7c
AikChun <aikchun616@gmail.com>
2014-05-05 03:39:14 +0800

    Removed computeDigests from hashtestscontroller

--- app/Controller/HashTestsController.php
+++ app/Controller/HashTestsController.php
@@ -200,32 +200,6 @@ class HashTestsController extends AppController {
 	}
 
 /**
- * Compute the hashed value of the input plaintext.
- * @param Array $selectedAlgorithms array-type $key => algorithm name
- * @param String $hashTestForm plaintext from the user
- * @return Array $output $key => array('HashAlgorithm' column names => value)
- */
-	protected function computeDigests($selectedAlgorithms, $hashTestForm) {
-		$computed = array();
-		$output = array();
-		foreach($selectedAlgorithms as $key => $algorithm ) {
-			$messageDigest = hash(strtolower($algorithm['HashAlgorithm']['name']), $hashTestForm['plaintext']);
-
-			$computed['HashResult']['plaintext'] = $hashTestForm['plaintext'];
-			$computed['HashResult']['message_digest'] = $messageDigest;
-			$computed['HashResult']['hash_algorithm_id'] = $algorithm['HashAlgorithm']['id'];
-			$computed['HashResult']['hash_algorithm_name'] = $algorithm['HashAlgorithm']['name'];
-			$computed['HashResult']['user_id'] = $hashTestForm['id'];
-			array_push($output, $computed);
-		}
-		// $output['HashAlgorithm'] = $selectedAlgorithms['HashAlgorithm'];
-		// $output['HashResult']['plaintext'] = $plaintext;
-		// $output['HashResult']['message_digest'] = $listOfMessageDigests;
-		
-		return $output;
-	}
-
-/**
  * Compare the message digests to come up with an analysis
  *
  */
